a178af6801a670d5623ea0608b1ecaf8cfee333a
kalo303 <khaloman303@gmail.com>
2022-06-20 09:28:45 -0700

    initial commit

---
+++ app/src/main/java/com/github/odaridavid/notesapp/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.github.odaridavid.notesapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private var yourThoughtsBody: String? = null
+    private var yourThoughtsTitle: String? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val thoughtsTitleEditText = findViewById<EditText>(R.id.enterYourThoughtsEditText)
+        val saveYourThoughtsButton = findViewById<Button>(R.id.saveYourThoughtsButton)
+        val viewYourThoughtsTextView = findViewById<TextView>(R.id.viewYourThoughtsTextView)
+        val thoughtsBodyEditText = findViewById<EditText>(R.id.thoughtsBody)
+        val mainLinearLayout = findViewById<LinearLayout>(R.id.main)
+
+        mainLinearLayout.setOnClickListener {
+            Intent(this, ImageActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
+
+        saveYourThoughtsButton.setOnClickListener {
+            yourThoughtsTitle = thoughtsTitleEditText.text.toString()
+            yourThoughtsBody = thoughtsBodyEditText.text.toString()
+            if (yourThoughtsTitle!!?.isNotBlank() && yourThoughtsBody!!.isNotBlank()) {
+                viewYourThoughtsTextView.text = yourThoughtsTitle + "\n" + yourThoughtsBody
+                viewYourThoughtsTextView.visibility = View.VISIBLE
+                thoughtsBodyEditText.setText("")
+                thoughtsTitleEditText.setText("")
+            }
+        }
+
+        viewYourThoughtsTextView.setOnClickListener {
+
+            if (yourThoughtsBody.isNullOrEmpty() || yourThoughtsTitle.isNullOrEmpty()) return@setOnClickListener
+
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra("title", yourThoughtsTitle)
+            intent.putExtra("body", yourThoughtsBody)
+            startActivity(intent)
+        }
+
+    }
+}
+
